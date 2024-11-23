1c103ae5f6acc49a8b7d8f35645b3f26221aa0c5
Kim-Mark <qsik1218000@gmail.com>
2024-11-23 20:51:50 +0900

    총알속도 20으로 증가 COIN-14

--- src/main/java/Invaders_SDP_server/service/GameService.java
+++ src/main/java/Invaders_SDP_server/service/GameService.java
@@ -47,14 +47,14 @@ public class GameService {
         {
             for(Bullet bullet : bullets)
             {
-                bullet.setY(bullet.getY()-20);
+                bullet.setY(bullet.getY()-20);//
             }
         }
         else//아래로
         {
             for(Bullet bullet : bullets)
             {
-                bullet.setY(bullet.getY()+20);
+                bullet.setY(bullet.getY()+20);//
             }
         }
     }
